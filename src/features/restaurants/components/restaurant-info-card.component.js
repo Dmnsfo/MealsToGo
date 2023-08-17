1fb93bc8d4c00638dfca145ef305ea94f58d88a2
Dmnsfo <sofiad.media@gmail.com>
2023-08-17 16:27:44 +0200

    fixed style, added isOpenedfunction

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,42 +4,63 @@ import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
 
 const Title = styled(Text)`
-  padding: ${(props) => props.theme.space[3]};
-  padding-bottom: ${(props) => props.theme.space[0]};
   font-size: ${(props) => props.theme.fontSizes.title};
   font-family: ${(props) => props.theme.fonts.heading};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const Address = styled(Text)`
   color: ${(props) => props.theme.colors.ui.primary};
+  font-size: ${(props) => props.theme.fontSizes.caption};
   font-family: ${(props) => props.theme.fonts.body};
-  padding: ${(props) => props.theme.space[3]};
-  padding-top: ${(props) => props.theme.space[1]};
 `;
 
-const Container = styled(View)`
-  flex: 1;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const Rating = styled(View)`
   flex-direction: row;
-  padding: ${(props) => props.theme.space[3]};
   padding-top: ${(props) => props.theme.space[1]};
   padding-bottom: ${(props) => props.theme.space[0]};
+`;
+
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction: row;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some restaurant",
-    //  icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100, some random street",
-    //   isOpenNow = true,
-    rating = 4,
+    isOpenNow = false,
+    rating = 5,
     //  isClosedTemporarely,
   } = restaurant;
 
@@ -47,22 +68,27 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
   return (
     <>
-      <Container>
-        <Card>
-          <Card.Cover
-            source={{
-              uri: photos[0],
-            }}
-          />
+      <RestaurantCard>
+        <RestaurantCardCover
+          source={{
+            uri: photos[0],
+          }}
+        />
+        <Info>
           <Title>{name}</Title>
-          <Rating>
-            {ratingArray.map(() => (
-              <SvgXml xml={star} width={20} height={20} />
-            ))}
-          </Rating>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => (
+                <SvgXml xml={star} width={20} height={20} />
+              ))}
+            </Rating>
+            <SectionEnd>
+              {isOpenNow && <Open xml={open} width={20} height={20} />}
+            </SectionEnd>
+          </Section>
           <Address variant="titleLarge">{address}</Address>
-        </Card>
-      </Container>
+        </Info>
+      </RestaurantCard>
     </>
   );
 };
