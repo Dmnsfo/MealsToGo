ec7f3c8794271caf148b2858aad3b7f60153755f
Dmnsfo <sofiad.media@gmail.com>
2023-08-20 08:34:01 +0200

    hoooked up the context

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { Text, View, FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, MD2Colors, Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -11,6 +11,16 @@ import { RestaurantContext } from "../../../services/restaurants/restaurants.con
 const SearchBox = styled(View)`
   padding: ${(props) => props.theme.space[3]};
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 
 const RestaurantList = styled(FlatList).attrs({
@@ -23,10 +33,15 @@ const RestaurantList = styled(FlatList).attrs({
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
-  const restaurantContext = useContext(RestaurantContext);
+  const { error, isLoading, restaurants } = useContext(RestaurantContext);
   return (
     <>
       <SafeContainer>
+        {isLoading && (
+          <LoadingContainer>
+            <Loading animating={true} color="#FF6347" size={40} />
+          </LoadingContainer>
+        )}
         <SearchBox>
           <Searchbar
             placeholder="Search"
@@ -36,10 +51,10 @@ export const RestaurantsScreen = () => {
         </SearchBox>
         <Spacer position="horizontal" size="medium">
           <RestaurantList
-            data={restaurantContext.restaurants}
-            renderItem={() => (
+            data={restaurants}
+            renderItem={({ item }) => (
               <Spacer position="bottom" size="large">
-                <RestaurantInfoCard />
+                <RestaurantInfoCard restaurant={item} />
               </Spacer>
             )}
             keyExtractor={(item) => item.name}
--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -4,8 +4,32 @@ import { restaurantsRequest, restaurantTransform } from "./restaurants.service";
 export const RestaurantContext = createContext();
 
 export const RestaurantContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantTransform)
+        .then((results) => {
+          setIsLoading(false);
+          setRestaurants(results);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
   return (
-    <RestaurantContext.Provider value={{ restaurants: [1, 2, 3, 4] }}>
+    <RestaurantContext.Provider value={{ restaurants, isLoading, error }}>
       {children}
     </RestaurantContext.Provider>
   );
