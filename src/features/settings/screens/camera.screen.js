e41177eb4476c73eaaf27b48a720e14d6cfe62a4
Dmnsfo <sofiad.media@gmail.com>
2023-09-22 20:46:16 +0200

    fixing the camera

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,34 +1,29 @@
 import React, { useRef, useState, useEffect, useContext } from "react";
+import { Camera } from "expo-camera";
 import styled from "styled-components/native";
-import { Camera, CameraType } from "expo-camera";
-import { Button, TouchableOpacity, View } from "react-native";
-import { Text } from "../../../components/typography/text.component";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { View, TouchableOpacity } from "react-native";
+import { Text } from "../../../components/typography/text.component";
+
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
+  flex: 1;
 `;
 
-const ToggleCamera = styled(TouchableOpacity)`
-  color: "#2182BD";
-  width: 200px;
-  height: 40px;
+const InnerSnap = styled.View`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
 `;
 
 export const CameraScreen = ({ navigation }) => {
-  const { user } = useContext(AuthenticationContext);
-  const cameraRef = useRef();
-  const [type, setType] = useState(CameraType.back);
   const [hasPermission, setHasPermission] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
+  const cameraRef = useRef();
+  const { user } = useContext(AuthenticationContext);
 
   const snap = async () => {
     if (cameraRef) {
@@ -38,32 +33,28 @@ export const CameraScreen = ({ navigation }) => {
     }
   };
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
   if (hasPermission === null) {
     return <View />;
   }
-
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
-  function toggleCameraType() {
-    setType((current) =>
-      current === CameraType.back ? CameraType.front : CameraType.back
-    );
-  }
-
   return (
-    <TouchableOpacity onPress={snap}>
-      <ProfileCamera
-        ref={(camera) => (cameraRef.current = camera)}
-        type={type}
-        ratio={"16:9"}
-      >
-        <TouchableOpacity onPress={snap} />
-        <ToggleCamera onPress={toggleCameraType}>
-          <Text>Flip Camera</Text>
-        </ToggleCamera>
-      </ProfileCamera>
-    </TouchableOpacity>
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+      ratio={"16:9"}
+    >
+      <TouchableOpacity onPress={snap}>
+        <InnerSnap />
+      </TouchableOpacity>
+    </ProfileCamera>
   );
 };
